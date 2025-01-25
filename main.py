41835d54975a3f91a6999838380e09ccc218f09a
VishvaAr <varasan@ucsb.edu>
2025-01-25 13:47:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # EIA API key and URL
 API_KEY = "olNnH6iSyPabk6zGefAKF6J3B5VW30dCmCMSr1Wh"
@@ -10,7 +13,7 @@ params = {
     "frequency": "monthly",
     "data[]": "value",
     "facets[product][]": "EPMR",
-    "facets[duoarea][0]": "Y05LA",  # Los Angeles
+    "facets[duoarea][0]": "Y05SF",  # Los Angeles
     "start": "2010",  # Start year for the range (change this as needed)
     "end": "2026",    # End year for the range (change this as needed)
 }
@@ -52,3 +55,36 @@ except requests.exceptions.RequestException as e:
     print(f"\nError fetching data from EIA API: {e}")
 except KeyError as e:
     print(f"\nError parsing response: Missing key {e}")
+
+
+storedPrices = []
+storedYears = []
+i =0
+for i in range(180):
+    storedPrices.append((float(sorted_prices[i]["value"])))
+    storedYears.append((sorted_prices[i]["period"][0:7]))
+#print(storedYears)
+#print(storedPrices)
+
+
+i = 0
+for year in range(2010,2025):
+    dateArray = []
+    priceArray = []
+    while i <= 11:
+        dateArray.append((sorted_prices[i + (year-2010)*12]["period"][5:7]))
+        priceArray.append((float(sorted_prices[i + (year-2010)*12]["value"])))
+        i += 1
+    
+    plt.xlabel('Dates')
+    plt.ylabel('Prices (Per Gallon)')
+    plt.title(f'Current Gas Prices in California (Year {year})')
+    plt.plot(dateArray, priceArray)
+    plt.show()
+    dateArray = []
+    priceArray = []
+    i=0
+
+
+
+
